09dc6d639afc668b7f2cc9e3f49c1a714a4c9b33
Vladimir Novopashin <novopashinwm@mail.ru>
2023-10-15 15:29:28 +0300

    +33

---
+++ DZ07_Parallelism/DZ07_Parallelism/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DZ07_Parallelism
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
